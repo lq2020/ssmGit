8ef6815c5dbbb13f8ab50a640201a1def2cb5040
lq2020 <181622014@qq.com>
2020-02-26 03:14:33 +0800

    项目第一次提交

---
+++ src/test/java/cn/sz/lq/test/Test.java
@@ -0,0 +1,11 @@
+package cn.sz.lq.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("hellojava");
+	}
+	public void show() {
+		System.out.println("nnah");
+	}
+}
